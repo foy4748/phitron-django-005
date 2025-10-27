6bd1c99d424126d28d3bcd33af61cadf2835ec8a
foy4748 <faisaljfcl@gmail.com>
2025-10-28 00:27:17 +0600

    Made Debuge True as default in project/settings.py

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.bool("DEBUG", default=True)
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
